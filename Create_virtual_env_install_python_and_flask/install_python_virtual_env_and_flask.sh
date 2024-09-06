f66ad161c09226b94edb5152f8ed20449d467931
Ubuntu <ubuntu@ip-172-31-95-59.ec2.internal>
2024-09-06 02:50:25 +0000

    Modified installation file into a script

---
+++ Create_virtual_env_install_python_and_flask/install_python_virtual_env_and_flask.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# 1. Update and Upgrade the System
+sudo apt-get update
+sudo apt-get upgrade -y
+
+# 2. Install Python
+sudo apt-get install python3 python3-pip -y
+
+# 3. Install Virtual Environment
+sudo apt-get install python3-venv -y
+
+# 4.  Create a Virtual Environment
+mkdir my_flask_app
+cd my_flask_app
+python3 -m venv venv
+
+# 5. Activate the Virtual Environment
+source venv/bin/activate
+
+# 6. Install Flask
+pip install Flask
+
+# 7. Verify Installation
+flask --version
+
+
+
