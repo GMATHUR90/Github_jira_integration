25a1e42d46a35cb50fa74b280dea09899faccb65
Gaurav Mathur <ecegauravmathur90@gmail.com>
2024-09-10 08:28:50 +0530

    Updated code to check if /jira is available then create a jira ticket on jira portal

--- Create_virtual_env_install_python_and_flask/my_flask_app/create_jira_ticket_app.py
+++ Create_virtual_env_install_python_and_flask/my_flask_app/create_jira_ticket_app.py
@@ -2,70 +2,85 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 
 # Creating a flask app instance
 app = Flask(__name__)
 
 @app.route('/createJira', methods=['POST'])
-#def hello():
-#    return "Hello World"
-#app.run('0.0.0.0')
+
 def createJIRA():
-  url = "https://ecegauravmathur90.atlassian.net/rest/api/3/issue"
-  email_id = os.getenv('EMAIL_ID')
-  jira_api_token = os.getenv('JIRA_API_TOKEN')
-  auth = HTTPBasicAuth(email_id, jira_api_token)
+  # Extract the JSON payload from the github webhook
+  data = request.json
 
-  headers = {
-    "Accept": "application/json",
-    "Content-Type": "application/json"
-  }
+  # Extract the comment from the webhook payload
+  comment_body = data.get('comment', {}).get('body', '')
 
-  payload = json.dumps( {
-    "fields": {
+  # Check if the comment contains the /jira command
+  if '/jira' in comment_body:
+    # URL to create a new Jira Issue 
+    url = "https://ecegauravmathur90.atlassian.net/rest/api/3/issue"
+
+    # Jira credential from environment variable
+    email_id = os.getenv('EMAIL_ID')
+    jira_api_token = os.getenv('JIRA_API_TOKEN')
+    auth = HTTPBasicAuth(email_id, jira_api_token)
+
+    # Jira issue details
+     
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+        "fields": {
     
-      "description": {
-        "content": [
-          {
+        "description": {
             "content": [
-              {
-                "text": "My first Jira Ticket",
-                "type": "text"
-              }
+            {
+                "content": [
+                {
+                    "text": "My first Jira Ticket",
+                    "type": "text"
+                }
+                ],
+                "type": "paragraph"
+            }
             ],
-            "type": "paragraph"
-          }
-        ],
-        "type": "doc",
-        "version": 1
-      },
+            "type": "doc",
+            "version": 1
+         },
     
-      "issuetype": {
-        "id": "10007"
-      },
+        "issuetype": {
+            "id": "10007"
+        },
     
     
-      "project": {
-        "key": "MTS"
-      },
+        "project": {
+            "key": "MTS"
+        },
     
-      "summary": "First Jira Ticket",
+        "summary": "First Jira Ticket",
     
-    },
-    "update": {}
-  } )
+        },
+        "update": {}
+    } )
+    # Make the request to create the Jira issue
+    response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+     )
+    # Return the Jira API response
+    #return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+    return jsonify(json.loads(response.text)), response.status_code
+  else:
+    # If comment does not contain /jira returan a message no action was taken
+    return jsonify({"message": "No Jira command found in the comment. No action taken."}) , 200
+    
 
-  response = requests.request(
-    "POST",
-    url,
-    data=payload,
-    headers=headers,
-    auth=auth
-  )
-
-  return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)  
-
-
